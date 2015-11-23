55687e8bb7b267122462dd2848a6b4d70eacb67e
Erik <enk@bu.edu>
2015-11-22 18:52:21 -0800

    First FPGA upload. Trying to blink LED, everything is set but need license

---
+++ simple_counter.v
@@ -0,0 +1,16 @@
+// Single clock input and a 32-bit output port
+
+module simple_counter (
+								CLOCK_50,
+								counter_out
+								);
+	
+input 				CLOCK_50;
+output	[31:0]	counter_out;
+reg		[31:0]	counter_out;
+
+always @ (posedge CLOCK_50)  // the positive edge of the clock
+	begin
+		counter_out <= #1 counter_out + 1;   // increment counter
+	end
+endmodule	// end of module simple_counter
